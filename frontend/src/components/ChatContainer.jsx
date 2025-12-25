400847bd54d37f7d4091841f11ec9c74bef84555
Sovit saini <sovitsaini37@gmail.com>
2025-12-25 11:18:50 +0530

    sovit

--- frontend/src/components/ChatContainer.jsx
+++ frontend/src/components/ChatContainer.jsx
@@ -3,8 +3,8 @@ import { useEffect, useRef } from "react";
 
 import ChatHeader from "./ChatHeader";
 import MessageInput from "./MessageInput";
-import MessageSkeleton from "../skeletons/MessageSkeleton";
-import { useAuthStore } from "../store/useAuthStore";
+import MessageSkeleton from "./skeletons/MessageSkeleton";
+import { useAuthStore } from "../store/UseAuthStore";
 import { formatMessageTime } from "../lib/utils";
 
 const ChatContainer = () => {
